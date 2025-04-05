8d28a46319c976e18dc8eb8468c6ea0a4e1cd9b3
BandaweJ <bands500@gmail.com>
2025-04-05 12:37:36 +0200

    removed red marks from report

--- src/app/finance/student-finance/billing/billing.component.ts
+++ src/app/finance/student-finance/billing/billing.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { feesActions } from '../../store/finance.actions';
+import { FeesModel } from '../../models/fees.model';
+import { selectFees } from '../../store/finance.selector';
 
 @Component({
   selector: 'app-billing',
   templateUrl: './billing.component.html',
-  styleUrls: ['./billing.component.css']
+  styleUrls: ['./billing.component.css'],
 })
-export class BillingComponent {
+export class BillingComponent implements OnInit {
+  fees$ = this.store.select(selectFees);
+  constructor(private store: Store) {
+    this.store.dispatch(feesActions.fetchFees());
+  }
 
+  ngOnInit(): void {}
 }
--- src/app/finance/student-finance/invoice/invoice.component.ts
+++ src/app/finance/student-finance/invoice/invoice.component.ts
@@ -6,6 +6,7 @@ import { selectedStudentInvoice } from '../../store/finance.selector';
 import { MatTableDataSource } from '@angular/material/table';
 import { BillModel } from '../../models/bill.model';
 import { SharedService } from 'src/app/shared.service';
+import { PaymentModel } from '../../models/payment.model';
 
 @Component({
   selector: 'app-invoice',
@@ -15,6 +16,8 @@ import { SharedService } from 'src/app/shared.service';
 export class InvoiceComponent implements OnInit {
   @Input() studentNumber: string | null = null;
   invoice$ = this.store.select(selectedStudentInvoice);
+  totalBill: number = 0;
+  totalPayments: number = 0;
 
   constructor(private store: Store, public sharedService: SharedService) {}
 
@@ -24,10 +27,36 @@ export class InvoiceComponent implements OnInit {
       this.store.dispatch(invoiceActions.fetchInvoice({ studentNumber }));
     }
     this.invoice$.subscribe((invoice) => {
-      if (invoice?.bills) this.billsDataSource.data = invoice?.bills;
+      if (invoice?.bills) {
+        this.billsDataSource.data = invoice?.bills;
+        this.totalBill = invoice.bills.reduce((total, bill) => {
+          if (bill.fees && typeof bill.fees.amount === 'number') {
+            return total + bill.fees.amount;
+          }
+          return total;
+        }, 0);
+      }
     });
   }
 
-  displayedColumns: string[] = ['class', 'term', 'fees', 'amount'];
-  billsDataSource!: MatTableDataSource<BillModel>;
+  billsDisplayedColumns: string[] = ['class', 'term', 'fees', 'amount'];
+  billsDataSource: MatTableDataSource<BillModel> =
+    new MatTableDataSource<BillModel>([]);
+
+  paymentsDisplayedColumns: string[] = [
+    'date',
+    'receiptno',
+    'method',
+    'amount',
+  ];
+  paymentsDataSource: MatTableDataSource<PaymentModel> =
+    new MatTableDataSource<PaymentModel>([]);
+
+  getTotalBill() {
+    return this.totalBill;
+  }
+
+  getTotalPayments() {
+    return this.totalPayments;
+  }
 }
