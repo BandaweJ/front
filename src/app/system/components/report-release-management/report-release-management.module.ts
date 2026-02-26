53f800f3fa26b171b5787a5d0a6b8d78fd456c92
BandaweJ <bands500@gmail.com>
2026-02-26 17:02:31 +0200

    fix: add Material Design modules to ReportReleaseManagementModule
    - Import all required Material Design modules
    - Add MatCardModule, MatButtonModule, MatIconModule, etc.
    - Fix missing Material module dependencies
    - Resolve component rendering issues in Academic Settings

--- src/app/system/components/report-release-management/report-release-management.module.ts
+++ src/app/system/components/report-release-management/report-release-management.module.ts
@@ -5,6 +5,23 @@ import { GenerateSessionsDialogComponent } from './generate-sessions-dialog/gene
 import { BulkUpdateDialogComponent } from './bulk-update-dialog/bulk-update-dialog.component';
 import { EditReleaseDialogComponent } from './edit-release-dialog/edit-release-dialog.component';
 
+// Material Design Modules
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 @NgModule({
   declarations: [
     ReportReleaseManagementComponent,
@@ -14,6 +31,21 @@ import { EditReleaseDialogComponent } from './edit-release-dialog/edit-release-d
   ],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatCheckboxModule,
   ],
   exports: [
     ReportReleaseManagementComponent,
